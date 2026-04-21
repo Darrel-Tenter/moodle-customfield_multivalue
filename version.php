c8219ed3d79c2e4f2e2a90904bf79609d9451b80
Darrel Tenter <78980762+Darrel-Tenter@users.noreply.github.com>
2026-04-20 22:26:39 -0600

    Bump version to 2026042001 (1.0.5) for report builder fix

--- version.php
+++ version.php
@@ -25,7 +25,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'customfield_multivalue';
-$plugin->version   = 2026032202;
+$plugin->version   = 2026042001;
 $plugin->requires  = 2024042200; // Moodle 4.4 minimum.
 $plugin->maturity  = MATURITY_STABLE;
-$plugin->release   = '1.0.4';
+$plugin->release   = '1.0.5';
